2b26ed67183110a34bcf2514c2857a45be55b3ed
Supremeimprints <108473180+Supremeimprints@users.noreply.github.com>
2023-04-04 15:54:59 +0300

    Create DeleteTxt.py

---
+++ DeleteTxt.py
@@ -0,0 +1,12 @@
+import os
+
+file_path = "/Users/user/Documents/BiglyBT Downloads"
+
+if os.path.exists(file_path):
+    for root, dirs, files in os.walk(file_path):
+        for file in files:
+            if file.endswith(".txt"):
+                os.remove(os.path.join(root, file))
+    print("All .txt files in", file_path, "and its subdirectories have been deleted.")
+else:
+    print(file_path, "does not exist.")
